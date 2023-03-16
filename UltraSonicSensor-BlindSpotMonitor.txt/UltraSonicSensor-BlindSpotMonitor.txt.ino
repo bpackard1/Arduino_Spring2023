dc1fe7350e19082f9b6e36f741b6959ffa5a64e0
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-03-16 14:09:25 -0400

    Initial Blind Spot Commit

---
+++ UltraSonicSensor-BlindSpotMonitor.txt/UltraSonicSensor-BlindSpotMonitor.txt.ino
@@ -0,0 +1,17 @@
+//Programmer: Brennan Packard
+//Date:/3/16/23
+//Program: Blind Spot Monitor
+
+/*
+ * This Program will determine distances approaching
+ * vehicle that are in your blind spot turn on a warning light
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
